7ab091e87715d9dfdeb51a4f5554fa2d06705833
pedroandika6200 <fedro.a62@gmail.com>
2020-10-06 12:51:40 +0700

    Membuat login.php

---
+++ pertemuan4/ceklogin.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['tombolsubmit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    if($username=="pedro" && $password=="123"){
+        echo "Sukses";
+    }
+    else{
+        echo "Username/Password anda salah";
+    }
+}
+else{
+    echo"Mohon maaf cek login tidak bisa diakses langsung";
+}
+?>
